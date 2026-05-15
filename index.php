6586b3d2ebb4751cb2326126a6bde59f9d88aec5
Reach Peng <pengreach123@gmail.com>
2026-05-15 16:35:28 +1000

    cleaned up code

--- index.php
+++ index.php
@@ -7,8 +7,6 @@
   <link rel="stylesheet" href="./styles/style.css">
   <title>Home - UrbanSync</title>
   <link rel="icon" type="image/x-icon" href="/images/logo.ico">
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="UrbanSync, A B2B company specializing in infrastructure analytics and improvement.">
   <meta name="author" content="Reach Peng, Liron Willathgamuwa, Dylan Kelly, MD Areen ">
 
