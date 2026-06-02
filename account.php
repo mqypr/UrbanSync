2674f221263661972027f9ef0d685a9cfe6ea896
Reach Peng <pengreach123@gmail.com>
2026-06-02 19:58:35 +1000

    final changes

--- account.php
+++ account.php
@@ -1,9 +1,6 @@
 <?php
 session_start();
 require_once './settings.php';
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
 
 $errors      = [];
 $success     = false;
